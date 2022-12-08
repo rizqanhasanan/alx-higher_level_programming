64d5d1f6e9933518783c1465ca86ecea3faa3583
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-12-07 21:55:07 -0800

    arrazy

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[matrix[i][j]**2 for j in range(len(matrix[0]))]
-            for i in range(len(matrix))]
+    new_matrix = [[j ** 2 for j in i] for i in matrix]
+    return (new_matrix)
