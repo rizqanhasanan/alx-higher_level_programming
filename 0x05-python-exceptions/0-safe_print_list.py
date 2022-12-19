cfe414200bbbaf59c5abe1bd36b495c4d9d02ea5
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-12-19 14:44:18 -0800

    my commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         count = 0
         for i in my_list[:x]:
             print(i, end='')
-            count += 1
+        count += 1
         print()
     except:
         print()
