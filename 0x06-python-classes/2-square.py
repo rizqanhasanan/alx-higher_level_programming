7f20e9bd1d190f42dabd420effc39dc8f93eda28
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-12-22 01:08:16 -0800

    commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-""" Class Square that defines a square """
-
-
-
 class Square:
-    """ Class Square """
+    """Class of the square"""
     def __init__(self, size=0):
-        """ Constructor """
-        if not isinstance(size, int):
+        """self.__size = size"""
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-                elif size < 0:
-            raise ValueError("size must be >= 0")                                    else:  
-                    self.__size = size
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
