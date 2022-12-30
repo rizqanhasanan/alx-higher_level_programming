ea0c445d579ea734d9e4205fc341faeb298adc0d
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-12-30 05:12:32 -0800

    commit

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if type(text) is not str:
+        raise TyeError("text must 
+be a string")
+
+    for i in ".?:":
+        text = text.replace(i, i + 
+"\n\n")
+    print('\n'.join(t.strip() for t 
+in text.split('\n')), end='')
---
+++ 0x07-python-test_driven_development/6-max_integer_test.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class 
+TestMaxInteger(unittest.TestCase):
+
+    def test_empty_list(self):
+                
+self.assertEqual(max_integer([]), 
+None)
+
+    def setUp(self):
+        pass
+
+    def test_all_same(self):
+            
+self.assertEqual(max_integer([0, 0,
+0]), 0)
+
+    def test_ordered_list(self):
+        ordered = [1, 2, 3, 4]
+                    
+self.assertEqual(max_integer(ordere
+d), 4)
+
+    def test_unordered_list(self):
+        unordered = [2, 3, 4, 1]
+                    
+self.assertEqual(max_integer(unorde
+red), 4)
+
+    def 
+test_single_element_list(self):
+        single_element_list = [18]
+                    
+self.assertEqual(max_integer(single
+_element_list), 18)
+
+    def test_negatives(self):
+        test_list = [-4, 0, 6, 4]
+                    
+self.assertEqual(max_integer(test_l
+ist), 6)
+
+       test_list = [-7, -6, -5, 
+-4]
+        
+self.assertEqual(max_integer(test_l
+ist), -4)
+
+    def test_max_integer(self):
+        test_list = [1, 2, 3, 9, 4]
+                    
+self.assertEqual(max_integer(test_l
+ist), 9)
+
+    def test_max_integer_neg(self):
+        test_list = [1, 2, 3, 4, 5, 
+-10, -100, -1000, 0]
+                    
+self.assertEqual(max_integer(test_l
+ist), 5)
+
+    def test_max_integer_(self):
+            
+self.assertEqual(max_integer([]), 
+None)
+
+    def test_positives(self):
+        test_list = [4, 2, 3, 1]
+                    
+self.assertEqual(max_integer(test_l
+ist), 4)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
