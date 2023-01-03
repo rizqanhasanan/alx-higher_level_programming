2277cd8bdbfbf494048f4cbf816d5ce60d0517d5
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2023-01-03 04:15:27 -0800

    commit

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# 0-rectangle.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a Rectangle class."""
+"""
+A rectangle.
+"""
 
 
 class Rectangle:
-        """Re
-        present a rectangle."""
-            pass
+    """
+    Rectangle functions and data
+    """
 
+    pass
